a7532a6e1bfeca61396a47da4f1c54642d50109c
rishuraj161228-creator <rishuraj161228@gmail.com>
2026-05-13 09:56:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from datetime import datetime
 API_KEY = "AIzaSyAF-LTCTYCI6X71tLktuwlL2xSRmNrJYJo" 
 
 # 2. Yahan apni ek Video ki ID dalein
-VIDEO_ID = "kS58hLVQAXU"
+VIDEO_ID = "HszK863ED-0"
 
 URL = f"https://www.googleapis.com/youtube/v3/videos?part=statistics&id={VIDEO_ID}&key={API_KEY}"
 
